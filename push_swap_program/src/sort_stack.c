f4ea19e2fd8e6095d04406441e3fcf5cc7f19092
jrandet <jrandet@student.42.fr>
2025-02-06 08:25:44 +0100

    index median reflexion written

--- push_swap_program/src/sort_stack.c
+++ push_swap_program/src/sort_stack.c
@@ -6,13 +6,13 @@
 /*   By: jrandet <jrandet@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/05 12:01:59 by jrandet           #+#    #+#             */
-/*   Updated: 2025/02/06 08:15:45 by jrandet          ###   ########.fr       */
+/*   Updated: 2025/02/06 08:25:24 by jrandet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/push_swap.h"
 
-/*static int	is_smallest_a(t_stack *stack)
+static int	is_smallest_a(t_stack *stack)
 {
 	int	*cursor;
 	int	current_min;
@@ -34,9 +34,9 @@
 		cursor++;
 	}
 	return (min_pos);
-}/*
+}
 
-/*void	sort(t_stack *stack, t_string *string)
+void	sort(t_stack *stack, t_string *string)
 {
 	int	rot_count;
 	
@@ -50,10 +50,14 @@
 	}
 	while (stack->top != stack->values)
 		push_a(stack, string);
-}*/
+}
 
 //with the indesx sorting, i can easily find the median as well as the values above or below it :)
 //explore the use of indexes and play around with this idea,
 //calculate the cost of the operations and move forward in that manner
 //then once you've found this strategy, rename then into pivot, and recursively split the array into smaller chunks
 
+// I can use len - 1 and the indexes to find the median value
+//once i have the median, then i can do all my operations
+//depending on where the median is, then it will change the procedure
+//calculate the cost of a ra move and the rra move and compare them one another. 
