bb0fdc7e2524be93ee7178a17c3f6f9df0dd1b47
jrandet <jrandet@student.42.fr>
2025-02-15 21:49:34 +0100

    jonas n'aime pas la valeur de mon pivot :'(

--- push_swap_program/src/sort_a.c
+++ push_swap_program/src/sort_a.c
@@ -6,22 +6,72 @@
 /*   By: jrandet <jrandet@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/15 18:35:24 by jrandet           #+#    #+#             */
-/*   Updated: 2025/02/15 19:28:42 by jrandet          ###   ########.fr       */
+/*   Updated: 2025/02/15 21:48:18 by jrandet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/push_swap.h"
 
 
+// /0 4 3 5 2 1 pivot = 5
+// /4 3 5 2 1 0 p = 0 o = 1
+// /3 5 2 1 0 4 p = 0 o = 2
+// /5 2 1 0 4 3 p = 0 o = 3
+// 5 / 2 1 0 4 3 p = 1 o = 4
+// 5 / 1 0 4 3 2 p = 1 o = 5
+// 5 / 0 4 3 2 1 p = 1 o = 6    len - pushed count 6 - 1 = 5  op = 6 
 
-void	sort(t_stack *stack, int len, int pivot_value, int *pushed_count, int *operations )
+// 5 / 1 0 4 3 2 rev rot a
+
+// 0 4 3 5 2 1 pivot = 3
+// 0 /4 3 5 2 1 p = 1 o = 1
+// 0/ 3 5 2 1 4 p = 1 o = 2
+// 0 /5 2 1 4 3 p = 1 o = 3
+// 0 /2 1 4 3 5 p = 1 o = 4
+// 0 2 /1 4 3 5 p = 2 o = 5
+// 0 2 1/ 4 3 5 p = 3 o = 6 
+
+//sort a
+// 0 2 1 /4 3 5 piv = 4
+// 0 2 1 / 3 5 4 p =n 0 o = 1
+// 0 2 1 3 / 5 4 len == 2
+
+//sort a 
+// 0 2 1 3/ 4 5 swap a
+
+
+void	sort(t_stack *stack, int len, int sub_len)
+{
+	int	pivot_value;
+	int	pushed_count;
+	int	ops_done_count;
+	int	pivot_i;
+
+	pivot_i = stack->top_i + sub_len;
+	pushed_count = 0;
+	ops_done_count = 0;
+	while ((pushed_count < sub_len) && (ops_done_count < len)) //they both start at 0 this is why i have the strictly smaller
+	{
+		if (*(stack->top) < pivot_i)
+		{
+			push_b(stack);
+			pushed_count++;
+		}
+		else
+			rot_a(stack);
+		ops_done_count++;
+	}
+	while (ops_done_count > (len - pushed_count))
+	{
+		rev_rot_a(stack);
+		ops_done_count--;
+	}
+}
 
 void	sort_a(t_stack *stack, int len)
 {
 	int	sub_len;
-	int	pivot_value;
-	int	pushed_count;
-	int	operations;
+	
 	
 	if (len <= 1)
 		return;
@@ -32,28 +82,7 @@ void	sort_a(t_stack *stack, int len)
 		return ;
 	}
 	sub_len = len / 2;
-	pivot_value = *(stack->top + sub_len);
-	pushed_count = 0;
-	operations = 0;
-
-	while ((pushed_count < sub_len) && (operations < len))
-	{
-		if (*(stack->top) < pivot_value)
-		{
-			push_b(stack);
-			pushed_count++;
-		}
-		else
-		{
-			rot_a(stack);
-		}
-		operations++;
-	}
-	while (operations > len - pushed_count)
-	{
-		rev_rot_a(stack);
-		operations--;
-	}
+	sort(stack, len, sub_len);
 	sort_a(stack, len - sub_len);
 	sort_b(stack, len);
 }
--- push_swap_program/src/sort_b.c
+++ push_swap_program/src/sort_b.c
@@ -1,35 +1,15 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   push.c                                             :+:      :+:    :+:   */
+/*   sort_b.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jrandet <jrandet@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/01/31 12:13:34 by jrandet           #+#    #+#             */
-/*   Updated: 2025/02/15 21:47:04 by jrandet          ###   ########.fr       */
+/*   Created: 2025/02/15 19:57:05 by jrandet           #+#    #+#             */
+/*   Updated: 2025/02/15 21:11:48 by jrandet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/push_swap.h"
 
-void	push_a(t_stack *stack)
-{
-	if (stack->top == stack->values)
-		return ;
-	stack->top--;
-	stack->top_i--;
-	stack->len_a++;
-	stack->len_b--;
-	ft_putstr("pa\n");
-}
-
-void	push_b(t_stack *stack)
-{
-	if (stack->end == (stack->top - 1))
-		return ;
-	stack->top++;
-	stack->top_i++;
-	stack->len_a--;
-	stack->len_b++;
-	ft_putstr("pb\n");
-}
+void	sort_b()
